ca820403409699a5e296f60276e79a50d1258118
AbdirahmanMoahemd <kaah6978@gmail.com>
2024-07-08 17:41:59 +0300

    from kaahiye

--- models/messagesModel.js
+++ models/messagesModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const messagesSchema = new mongoose.Schema({
   sender: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: "users",
     required: true,
   },
   receiver: {
